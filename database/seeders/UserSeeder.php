b594994721009fc0e15392c19cb0fcec647440a2
jhomarchicas <jhomarchicas7@gmail.com>
2026-09-22 19:23:40 -0600

    Configurar seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Cliente;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -10,28 +11,70 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Super Usuario
-        User::create([
-            'name' => 'Jhomar Caleb',
-            'email' => 'cchicas@gmail.com',
-            'password' => Hash::make('User2026'),
-            'role' => 'Super Usuario',
-        ]);
+        /*
+        |--------------------------------------------------------------------------
+        | Super Usuario
+        |--------------------------------------------------------------------------
+        */
 
-        // Gestor de Cobros
-        User::create([
-            'name' => 'Gestor de Cobros',
-            'email' => 'gestor@gmail.com',
-            'password' => Hash::make('User2026'),
-            'role' => 'Gestor de Cobros',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'cchicas@gmail.com'],
+            [
+                'name' => 'Jhomar Caleb',
+                'password' => Hash::make('User2026'),
+                'role' => 'Super Usuario',
+            ]
+        );
 
-        // Cliente
-        User::create([
-            'name' => 'Cliente Demo',
-            'email' => 'cliente@gmail.com',
-            'password' => Hash::make('User2026'),
-            'role' => 'Cliente',
-        ]);
+        /*
+        |--------------------------------------------------------------------------
+        | Gestor de Cobros
+        |--------------------------------------------------------------------------
+        */
+
+        User::firstOrCreate(
+            ['email' => 'owen@gmail.com'],
+            [
+                'name' => 'Owen Torres',
+                'password' => Hash::make('1234'),
+                'role' => 'Gestor de Cobros',
+            ]
+        );
+
+        /*
+        |--------------------------------------------------------------------------
+        | Cliente
+        |--------------------------------------------------------------------------
+        | Primero creamos el registro en la tabla clientes.
+        */
+
+        $cliente = Cliente::firstOrCreate(
+            ['documento_identidad' => '01234567-8'],
+            [
+                'nombres' => 'Katherine Andrea',
+                'apellidos' => 'Ramos Carranza',
+                'telefono' => '70000000',
+                'correo' => 'andrea@gmail.com',
+                'direccion' => 'Zacatecoluca',
+                'estado' => 'Activo',
+            ]
+        );
+
+        /*
+        |--------------------------------------------------------------------------
+        | Usuario del Cliente
+        |--------------------------------------------------------------------------
+        | Vinculamos el usuario con el cliente mediante cliente_id.
+        */
+
+        User::firstOrCreate(
+            ['email' => 'andrea@gmail.com'],
+            [
+                'name' => 'Katherine Andrea Ramos Carranza',
+                'password' => Hash::make('1234'),
+                'role' => 'Cliente',
+                'cliente_id' => $cliente->id,
+            ]
+        );
     }
 }
